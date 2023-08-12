7f257b018f6980a70dd7ae90d0ef82b1ca5c3e85
RomuloRamos <romulo.ramos@gee.inatel.br>
2023-08-12 14:19:30 -0300

    <ci-fix> Test

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,7 @@ import { themes } from '@storybook/theming'
 import { InitializeOptions, initialize, mswLoader } from 'msw-storybook-addon';
 
 import '../src/styles/global.css'
+import { setupWorker } from "msw";
 
 
 let options: InitializeOptions = {
@@ -15,6 +16,18 @@ if(location.hostname === 'https://romuloramos.github.io') {
       url:"https://romuloramos.github.io/ignite_ds_lab/mockServiceWorker.js"
     } 
   }
+  console.log("Agora é server")
+}
+else{
+  const worker = setupWorker()
+
+  // worker.start({
+  //   serviceWorker: {
+  //     // Points to the custom location of the Service Worker file.
+  //     url: '/assets/mockServiceWorker.js'
+  //   }
+  // })
+  console.log("Não é server, é local")
 }
  
 // Initialize MSW
